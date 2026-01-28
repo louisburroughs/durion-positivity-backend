002850b7059aab395ec26e9fbbcf206b56949d63
louisburroughs <louis.burroughs@gmail.com>
2026-01-28 16:24:36 -0500

    feat: Update application configurations and refactor logging structure across multiple services

--- pos-event-receiver/src/main/java/com/positivity/poseventreceiver/PosEventReceiverApplication.java
+++ pos-event-receiver/src/main/java/com/positivity/poseventreceiver/PosEventReceiverApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PosEventreceiverApplication {
+public class PosEventReceiverApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PosEventreceiverApplication.class, args);
+        SpringApplication.run(PosEventReceiverApplication.class, args);
     }
 }
