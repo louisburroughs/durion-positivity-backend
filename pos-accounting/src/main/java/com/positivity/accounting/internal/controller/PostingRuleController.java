6c1a9bfa8a5127c164b0236d6105aad55783049c
louisburroughs <louis.burroughs@gmail.com>
2026-02-12 03:55:25 -0500

    Fixing test failures

--- pos-accounting/src/main/java/com/positivity/accounting/internal/controller/PostingRuleController.java
+++ pos-accounting/src/main/java/com/positivity/accounting/internal/controller/PostingRuleController.java
@@ -1,17 +1,8 @@
 package com.positivity.accounting.internal.controller;
 
-import com.positivity.accounting.internal.dto.PostingRuleSetCreateRequest;
-import com.positivity.accounting.internal.dto.PostingRuleSetListResponse;
-import com.positivity.accounting.internal.dto.PostingRuleSetResponse;
-import com.positivity.accounting.internal.dto.PostingRuleVersionResponse;
-import com.positivity.accounting.service.PostingRuleService;
-import com.positivity.events.EmitEvent;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -27,9 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.positivity.accounting.internal.dto.PostingRuleSetCreateRequest;
+import com.positivity.accounting.internal.dto.PostingRuleSetListResponse;
+import com.positivity.accounting.internal.dto.PostingRuleSetResponse;
+import com.positivity.accounting.internal.dto.PostingRuleVersionResponse;
+import com.positivity.accounting.service.PostingRuleService;
+import com.positivity.events.EmitEvent;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 
 /**
  * REST Controller for Posting Rule Set operations.
