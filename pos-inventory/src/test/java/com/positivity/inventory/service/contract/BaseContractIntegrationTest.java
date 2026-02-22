83baa672d6e7339fd172c540cc59761e4bc2ee25
louisburroughs <louis.burroughs@gmail.com>
2026-02-22 15:56:09 -0500

    fix: update X-Authorities header in BaseContractIntegrationTest to include all necessary permissions

--- pos-inventory/src/test/java/com/positivity/inventory/service/contract/BaseContractIntegrationTest.java
+++ pos-inventory/src/test/java/com/positivity/inventory/service/contract/BaseContractIntegrationTest.java
@@ -13,6 +13,12 @@ public abstract class BaseContractIntegrationTest {
     protected MockHttpServletRequestBuilder withGatewayAuth(MockHttpServletRequestBuilder requestBuilder) {
         return requestBuilder
                 .header("X-User", "contract-test-user")
-                .header("X-Authorities", "inventory:availability:read");
+                .header("X-Authorities",
+                        String.join(",",
+                                "inventory:availability:read",
+                                "inventory:adjustment:create",
+                                "inventory:adjustment:approve",
+                                "inventory:location:write",
+                                "inventory:picking:manage"));
     }
 }
