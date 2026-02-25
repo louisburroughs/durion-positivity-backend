296af8aee3f4d2b59845be3da00e2e881c2fa719
louisburroughs <louis.burroughs@gmail.com>
2026-02-25 11:07:12 -0500

    Updating entites to manage auditing fields

--- pos-location/src/main/java/com/positivity/location/internal/entity/StorageLocationEntity.java
+++ pos-location/src/main/java/com/positivity/location/internal/entity/StorageLocationEntity.java
@@ -5,17 +5,22 @@ import com.positivity.location.internal.enums.StorageLocationStatus;
 import com.positivity.shared.id.UUIDv7Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import java.time.Instant;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
  * Storage location aggregate for floor/shelf/bin/cage/truck topology.
@@ -28,6 +33,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class StorageLocationEntity {
 
     @Id
@@ -62,6 +68,14 @@ public class StorageLocationEntity {
 
     @Column(name = "temperature", columnDefinition = "TEXT")
     private String temperature;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
 
     @PrePersist
     void onCreate() {
