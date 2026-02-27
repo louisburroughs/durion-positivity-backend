ecd2a2e657ea9ad5dbb2f869a7a2b7ea30758e41
louisburroughs <louis.burroughs@gmail.com>
2026-02-27 16:39:55 -0500

    fix(sonar): 02272127-20fa remediate safe issues

--- pos-location/src/main/java/com/positivity/location/internal/entity/ServiceAreaEntity.java
+++ pos-location/src/main/java/com/positivity/location/internal/entity/ServiceAreaEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.HashSet;
