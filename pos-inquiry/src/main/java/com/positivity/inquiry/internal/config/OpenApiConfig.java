f9663d9056e01e9279a86402b0f84ac85ff1e204
Louis Burroughs <louis.burroughs@gmail.com>
2026-03-16 22:38:34 -0400

    feat: Add OpenAPI configuration and initial YAML documentation for Positivity Inquiry API

---
+++ pos-inquiry/src/main/java/com/positivity/inquiry/internal/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.positivity.inquiry.internal.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Positivity Inquiry API")
+                        .version("v1")
+                        .description("API documentation for the Inquiry service")
+                        .contact(new Contact()
+                                .email("louis.burroughs@gmail.com")
+                                .name("Durion Team")));
+    }
+}
