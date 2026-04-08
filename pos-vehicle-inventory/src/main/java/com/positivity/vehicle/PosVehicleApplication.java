fb88cdb3bc00cef8c6b52cc725583cb598cc48bb
Louis Burroughs <louis.burroughs@gmail.com>
2026-04-08 13:45:11 -0400

    feat: change Hibernate ddl-auto setting from validate to update across multiple services

--- pos-vehicle-inventory/src/main/java/com/positivity/vehicle/PosVehicleApplication.java
+++ pos-vehicle-inventory/src/main/java/com/positivity/vehicle/PosVehicleApplication.java
@@ -2,8 +2,9 @@ package com.positivity.vehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class PosVehicleApplication {
     public static void main(String[] args) {
         SpringApplication.run(PosVehicleApplication.class, args);
