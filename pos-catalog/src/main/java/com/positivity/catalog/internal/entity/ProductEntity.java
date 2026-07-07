346476bdef1258ec04005c465f430849de3db150
Louis Burroughs <louis.burroughs@michelin.com>
2026-07-07 09:50:39 -0400

    Moved annotation

--- pos-catalog/src/main/java/com/positivity/catalog/internal/entity/ProductEntity.java
+++ pos-catalog/src/main/java/com/positivity/catalog/internal/entity/ProductEntity.java
@@ -36,7 +36,6 @@ public class ProductEntity implements CatalogItem {
     private UUID id;
 
     @PrePersist
-    @PostLoad
     public void generateId() {
         if (status == null) {
             status = ProductStatus.ACTIVE;
