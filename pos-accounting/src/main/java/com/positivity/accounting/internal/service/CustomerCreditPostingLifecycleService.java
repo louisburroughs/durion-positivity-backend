746c9428672f16071b09c43c36e2844490828c4f
Louis Burroughs <louis.burroughs@gmail.com>
2026-08-24 17:09:39 -0400

    Trying to fix imports

--- pos-accounting/src/main/java/com/positivity/accounting/internal/service/CustomerCreditPostingLifecycleService.java
+++ pos-accounting/src/main/java/com/positivity/accounting/internal/service/CustomerCreditPostingLifecycleService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
